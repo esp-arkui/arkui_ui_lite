a0e80e0340ad33ca038e90bcd7caec366d3ec4a8
xvcheng1979101 <xvcheng1979@qq.com>
2021-11-26 16:05:42 +0800

    代码提交处理

--- test/uitest/test_canvas/ui_test_canvas.cpp
+++ test/uitest/test_canvas/ui_test_canvas.cpp
@@ -676,7 +676,7 @@ void UITestCanvas::UIKitCanvasTestDrawMulitLangueText001()
     fontStyle.fontSize = 15;    // 30: font size
     fontStyle.letterSpace = 2; // 10 letter space
     Paint paint;
-    canvas->SetRotate(10,paint);
+    //canvas->SetRotate(10,paint);
     paint.SetFillColor(Color::GetColorFromRGBA(255,255,0,255));
     canvas->StrokeText("英语：hello word HMOS.", {0,20}, fontStyle, paint);
     canvas->StrokeText("日语：こんにちは、おーとり鴻もー蒙", {0,40}, fontStyle, paint);
