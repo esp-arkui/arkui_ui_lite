8665ada2b729e782ec91a814d8a25bfd5f0db68c
yiny <438563755@qq.com>
2022-01-10 09:34:44 +0800

    注释修改

--- frameworks/components/ui_canvas.cpp
+++ frameworks/components/ui_canvas.cpp
@@ -22,7 +22,7 @@
 #include "draw/draw_arc.h"
 #include "draw/draw_image.h"
 #include "gfx_utils/graphic_log.h"
-
+#include <ctime>
 namespace OHOS {
     void UICanvas::BeginPath()
     {
@@ -991,6 +991,13 @@ namespace OHOS {
         cordsTmp.SetPosition(start.x, start.y);
         cordsTmp.SetHeight(imageParam->height);
         cordsTmp.SetWidth(imageParam->width);
+        uint8_t *image = (uint8_t *)malloc(imageParam->height * imageParam->width * 4);
+        clock_t start1, end;   //clock_t 是clock()的返回变量类型
+            start1 = clock();
+        FastBoxBlur::BoxBlur((uint8_t*)imageParam->image->GetImageInfo()->data,image,
+                             imageParam->width,imageParam->height,imageParam->width * 4,5);
+        end = clock();   //捕捉循环段结束的时间
+        printf("use time: %f \n",(end-start1) / CLK_TCK);
 
         if (paint.GetChangeFlag()) {
             TransAffine transform;
@@ -1000,13 +1007,21 @@ namespace OHOS {
             transform.Translate(imageParam->start.x, imageParam->start.y);
             RenderingBuffer imageRendBuffer;
             uint8_t pxSize = DrawUtils::GetPxSizeByColorMode(imageParam->image->GetImageInfo()->header.colorMode);
-            imageRendBuffer.Attach((unsigned char*)imageParam->image->GetImageInfo()->data, imageParam->width,
-                                   imageParam->height, imageParam->width * (pxSize >> OHOS::PXSIZE2STRIDE_FACTOR));
+//            imageRendBuffer.Attach((unsigned char*)imageParam->image->GetImageInfo()->data, imageParam->width,
+//                                   imageParam->height, imageParam->width * (pxSize >> OHOS::PXSIZE2STRIDE_FACTOR));
+            imageRendBuffer.Attach(image, imageParam->width,
+                                              imageParam->height, imageParam->width * (pxSize >> OHOS::PXSIZE2STRIDE_FACTOR));
+
             DoRenderImage(renderBuffer, paint, invalidatedArea, transform, imageRendBuffer);
         } else {
-            DrawImage::DrawCommon(gfxDstBuffer, cordsTmp, invalidatedArea, imageParam->image->GetImageInfo(), style,
-                                  paint.GetOpacity());
+//            DrawImage::DrawCommon(gfxDstBuffer, cordsTmp, invalidatedArea, imageParam->image->GetImageInfo(), style,
+//                                  paint.GetOpacity());
+            OpacityType opa = DrawUtils::GetMixOpacity(paint.GetOpacity(), style.imageOpa_);
+            uint8_t pxBitSize = DrawUtils::GetPxSizeByColorMode(ARGB8888);
+            DrawUtils::GetInstance()->DrawImage(gfxDstBuffer, cordsTmp, invalidatedArea, image, opa, pxBitSize,
+                                                static_cast<ColorMode>(ARGB8888));
         }
+        free(image);
     }
 #endif
     void UICanvas::DoDrawLabel(BufferInfo& gfxDstBuffer,
